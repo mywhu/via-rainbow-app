c7676bc713521c4ae45b77c240c2f24c71e8fb21
EqualMa <equalma@outlook.com>
2020-05-04 08:32:45 +0000

    deploy

---
+++ precache-manifest.c252b09dbb83cbfab956da0cf5e76200.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6002d7b0ea85da348bea",
+    "url": "/css/chunk-078a426a.f270e4e1.css"
+  },
+  {
+    "revision": "d5bf93a6664dce0d5d36",
+    "url": "/css/chunk-vendors.bd59a40f.css"
+  },
+  {
+    "revision": "d2c646f88c2e035e37f0",
+    "url": "/css/home.e43d4617.css"
+  },
+  {
+    "revision": "09ea388ca4216b3a37fa",
+    "url": "/css/home~me~sign-up-code~user.eb57dd1e.css"
+  },
+  {
+    "revision": "28296ed9fb51dbfe1621",
+    "url": "/css/login~register.e8810fe7.css"
+  },
+  {
+    "revision": "04ecb8ac908808246008",
+    "url": "/css/me.ebd2e3b5.css"
+  },
+  {
+    "revision": "65f344a7afb662eb390d",
+    "url": "/css/sign-up-code.c089e800.css"
+  },
+  {
+    "revision": "312996e1b231e6c8140d24ccbc824d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "605cc8a01fa915f93f67",
+    "url": "/js/about.ea643a30.js"
+  },
+  {
+    "revision": "3d1b551ff7ed4880f850",
+    "url": "/js/app.cf6847a5.js"
+  },
+  {
+    "revision": "6002d7b0ea85da348bea",
+    "url": "/js/chunk-078a426a.d5f1bf8b.js"
+  },
+  {
+    "revision": "5534dc67156e84b95cda",
+    "url": "/js/chunk-2d0a389e.4055f7e0.js"
+  },
+  {
+    "revision": "f574ea92c876866e771a",
+    "url": "/js/chunk-2d0d7889.fec9dc5e.js"
+  },
+  {
+    "revision": "d5bf93a6664dce0d5d36",
+    "url": "/js/chunk-vendors.11ef063f.js"
+  },
+  {
+    "revision": "d2c646f88c2e035e37f0",
+    "url": "/js/home.55491d22.js"
+  },
+  {
+    "revision": "09ea388ca4216b3a37fa",
+    "url": "/js/home~me~sign-up-code~user.29f1fca5.js"
+  },
+  {
+    "revision": "a7cd10d48c3407ddedad",
+    "url": "/js/login.69f8b36f.js"
+  },
+  {
+    "revision": "28296ed9fb51dbfe1621",
+    "url": "/js/login~register.98245b63.js"
+  },
+  {
+    "revision": "04ecb8ac908808246008",
+    "url": "/js/me.f0f56c9b.js"
+  },
+  {
+    "revision": "b10edf48bd5fd30dafd6",
+    "url": "/js/register.84143da3.js"
+  },
+  {
+    "revision": "65f344a7afb662eb390d",
+    "url": "/js/sign-up-code.6c4fd5e1.js"
+  },
+  {
+    "revision": "f40b615ba6d0e7e1cfb5",
+    "url": "/js/user.46bc02ab.js"
+  },
+  {
+    "revision": "d5f1e9f4573c0eb3d70c",
+    "url": "/js/vendor-vue.c4955000.js"
+  },
+  {
+    "revision": "4eb0bb80490211ee4872",
+    "url": "/js/via-rainbow-core.4b475add.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+
+importScripts(
+  "/precache-manifest.c252b09dbb83cbfab956da0cf5e76200.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "app"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
